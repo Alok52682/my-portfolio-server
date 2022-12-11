ab9c61d5b30d1c9c1ed0f1873a3690a7a201dc70
Alok <alok@Aloks-MacBook-Air.local>
2022-12-11 15:39:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const allProjects = require('./AllProjects.json');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    res.send('server is running');
+})
+app.get('/projects/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const phone = allProjects.find(project => project.id == id) || {};
+
+    res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+})
